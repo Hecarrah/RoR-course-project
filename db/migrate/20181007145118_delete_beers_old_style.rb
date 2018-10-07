64010fa1ef93bf1ce2a5424d571e8410d68a3a6d
Peter Porttinen <pete.porttinen@gmail.com>
2018-10-07 20:08:09 +0300

    fix

--- db/migrate/20181007145118_delete_beers_old_style.rb
+++ db/migrate/20181007145118_delete_beers_old_style.rb
@@ -1,7 +1,7 @@
 class DeleteBeersOldStyle < ActiveRecord::Migration[5.1]
   def change
     change_table :beers do |t|
-      t.remove :oldStyle, :string
+      t.remove :oldStyle
     end
   end
 end
